28869aaedd40821213c63e4433609b19a8cd4b19
luokaihui <luokaihui_i@didichuxing.com>
2018-01-23 19:16:18 +0800

    完善代码

--- src/RayrDropdown.js
+++ src/RayrDropdown.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-// import 'RayrDropdown.scss';
+import $_ajax from 'ajax.js';
 
 class RayrDropdown extends React.Component {
     constructor(props){
